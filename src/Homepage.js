eb95ac0ddc4eab63db177f543641f9b5013673a6
Vishwas_kadam642 <vishwaskadam642@gmail.com>
2026-04-07 22:02:32 +0530

    added google href in homepage

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,3 +1,5 @@
+import React from "react";
+
 function Homepage() {
   return (
     <div>
@@ -7,4 +9,29 @@ function Homepage() {
   );
 }
 
+function customRender(reactElement, container) {
+  const domElement = document.createElement(reactElement.type);
+
+  domElement.innerHTML = reactElement.props.children;
+
+  domElement.setAttribute('href', reactElement.props.href);
+  domElement.setAttribute('target', reactElement.props.target);
+
+  container.appendChild(domElement);
+}
+
+const reactElement = React.createElement(
+  'a',
+  {
+    href: 'https://www.google.com',
+    target: '_blank',
+  },
+  'Google'
+);
+
+const mainContainer = document.getElementById('root');
+
+customRender(reactElement, mainContainer);
+
 export default Homepage;
+export { customRender };
